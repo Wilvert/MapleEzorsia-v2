095c56af5f02beae294f6fe7ad3338787a689286
Wilvert <we.blaauwendraad@gmail.com>
2025-08-31 21:06:08 +0200

    Revert "Merge branch 'main' of https://github.com/Wilvert/MapleEzorsia-v2"
    This reverts commit 75ab4e001ca8ab78cccc65efcf70cddb1a7c8333, reversing
    changes made to 8b6b8f396cba290697724a9c9c857b9769547bc2.

--- ezorsia/dllmain.cpp
+++ ezorsia/dllmain.cpp
@@ -71,7 +71,6 @@ void MainFunc() {
 
 	std::cout << "Applying resolution " << Client::m_nGameWidth << "x" << Client::m_nGameHeight << std::endl;
 	Client::UpdateResolution();
-	Client::ExpandItem();
 
 	dinput8::CreateHook();	std::cout << "dinput8 hook initialized" << std::endl;
 }
